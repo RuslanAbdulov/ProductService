381644e18dc9465265db0d1b8b1def178c0d15f3
rabdulov <ratioff@list.ru>
2019-06-28 01:20:23 +0400

    hystrix settings and dashboard

--- src/main/java/com/gridu/scalable/be/catalog/repository/CatalogRepository.java
+++ src/main/java/com/gridu/scalable/be/catalog/repository/CatalogRepository.java
@@ -1,10 +1,14 @@
 package com.gridu.scalable.be.catalog.repository;
 
 import com.gridu.scalable.be.catalog.domain.Product;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
@@ -18,6 +22,7 @@ public class CatalogRepository {
         this.restTemplate = restTemplate;
     }
 
+    @HystrixCommand(commandKey = "catalog-by-id", fallbackMethod = "catalogUnavailableById")
     public Optional<Product> findById(String id) {
         ResponseEntity<Product> response =
                 restTemplate.exchange("http://catalog-service/api/catalog/products/{id}",
@@ -29,6 +34,7 @@ public class CatalogRepository {
         return Optional.ofNullable(response.getBody());
     }
 
+    @HystrixCommand(commandKey = "catalog-by-sku", fallbackMethod = "catalogUnavailableBySku")
     public List<Product> findBySku(String sku) {
         ResponseEntity<List<Product>> response =
                 restTemplate.exchange("http://catalog-service/api/catalog/products?sku={sku}",
@@ -38,6 +44,16 @@ public class CatalogRepository {
                         sku);
 
         return response.getBody();
+    }
+
+    @SuppressWarnings("unused")
+    private Optional<Product> catalogUnavailableById(String id) {
+        throw new HttpServerErrorException(HttpStatus.SERVICE_UNAVAILABLE, "Catalog service unavailable");
+    }
+
+    @SuppressWarnings("unused")
+    private List<Product> catalogUnavailableBySku(String sku) {
+        throw new HttpServerErrorException(HttpStatus.SERVICE_UNAVAILABLE, "Catalog service unavailable");
     }
 
 }
--- src/main/java/com/gridu/scalable/be/catalog/repository/InventoryRepository.java
+++ src/main/java/com/gridu/scalable/be/catalog/repository/InventoryRepository.java
@@ -1,10 +1,14 @@
 package com.gridu.scalable.be.catalog.repository;
 
 import com.gridu.scalable.be.catalog.domain.ProductInventory;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
@@ -22,6 +26,7 @@ public class InventoryRepository {
         return findByIds(Collections.singletonList(id)).stream().findFirst();
     }
 
+    @HystrixCommand(commandKey = "inventory-by-ids", fallbackMethod = "inventoryUnavailable")
     public List<ProductInventory> findByIds(List<String> ids) {
         ResponseEntity<List<ProductInventory>> response =
                 restTemplate.exchange("http://inventory-service/api/inventory?uniq_ids={uniq_ids}",
@@ -31,6 +36,11 @@ public class InventoryRepository {
                         String.join(",", ids));
 
         return response.getBody();
+    }
+
+    @SuppressWarnings("unused")
+    private List<ProductInventory> inventoryUnavailable(List<String> ids) {
+        throw new HttpServerErrorException(HttpStatus.SERVICE_UNAVAILABLE, "Inventory service unavailable");
     }
 
 }
